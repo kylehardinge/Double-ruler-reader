0b38b02be41b3e68c329d165c8890f0743a3f405
Kyle Hardinge <kyle@hardinge.net>
2020-11-26 21:06:30 -0500

    More work on the slider

--- slider.js
+++ slider.js
@@ -1,13 +1,14 @@
-function Slider(s, pb, mb) { 
+export default function Slider(s, pb, mb) { 
     var slider = document.getElementById(s);
     var plus_button = document.getElementById(pb);
     var minus_button = document.getElementById(mb);
     var slider_value = slider.value;
+
     plus_button.addEventListener("click", () => {
-        slider_value += 1;
-    })
+        slider_value = slider_value + 1;
+    });
     minus_button.addEventListener("click", () => {
-        slider_value -= 1;
-    })
+        slider_value = slider_value - 1;
+    });
     
 }
