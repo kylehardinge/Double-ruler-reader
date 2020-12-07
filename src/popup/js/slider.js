8f41b858f703009158db34c20b30ac0c2c5bc9fb
Kyle Hardinge <kyle@hardinge.net>
2020-12-06 21:07:05 -0500

    Refactored files to be more organized

--- src/popup/js/slider.js
+++ src/popup/js/slider.js
@@ -1,14 +1,17 @@
-export default function Slider(s, pb, mb) { 
+export default function Slider(s, pb, mb, v) { 
     var slider = document.getElementById(s);
     var plus_button = document.getElementById(pb);
     var minus_button = document.getElementById(mb);
+    var value = document.getElementById(v);
     var slider_value = slider.value;
 
     plus_button.addEventListener("click", () => {
         slider_value = slider_value + 1;
+        value.innerText = slider_value;
     });
     minus_button.addEventListener("click", () => {
         slider_value = slider_value - 1;
+        value.innerText = slider_value;
     });
     
 }
